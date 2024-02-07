a641895a2fe58121bbafdd7e2c84b10c094333aa
Luis Salcido <salcidogrijalva@outlook.com>
2024-02-07 14:44:29 -0700

    Adding clean arquitecture to TokTik app

---
+++ lib/infrastructure/repositories/video_posts_repository.dart
@@ -0,0 +1,19 @@
+import 'package:toktik/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+
+class VideoPostsRepository implements VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepository({required this.videosDatasource});
+
+  @override
+  Future<List<VideoPost>> getFavoritesVideosByUser(String userId) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+}
--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  final VideoPostRepository videosRepository;
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
+  DiscoverProvider({required this.videosRepository});
+
   Future<void> loadNextPage() async {
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity())
-        .toList();
+    final newVideos = await videosRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
